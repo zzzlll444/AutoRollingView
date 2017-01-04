19818eb6892cc128a0939dfeb1d48f7072d873be
bo <2697768525@qq.com>
2017-01-04 19:57:57 +0800

    second commit

--- CustomRollingView/UIView+EidanSize.h
+++ CustomRollingView/UIView+EidanSize.h
@@ -1,9 +1,9 @@
 //
-//  UIView+EidanSize.h
+//  UIView+boSize.h
 //  SlideOutTest7
 //
-//  Created by Eidan Lin on 12-11-7.
-//  Copyright (c) 2012年 taomm. All rights reserved.
+//  Created by bo on 12-11-7.
+//  Copyright (c) 2012年 qdingnet. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
